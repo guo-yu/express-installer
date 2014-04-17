14ce87cd3348722eb735c1bb011a6500fedf8c72
Guo Yu <i@guoyu.me>
2014-04-17 14:16:56 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var configCtrler = require('./ctrlers/config');
+
+module.exports = function(app, configModel) {
+  var config = configCtrler(configModel);
+  return function(req, res, next) {
+    if (app.get('configed')) return next();
+    config.check(function(err, installed, configs) {
+      if (err) return next(err);
+      if (installed) {
+        app.locals.site = configs;
+        app.enable('configed');
+        return next();
+      } else {
+        config.create(app.locals.site, function(err, c) {
+          if (!err) app.enable('configed');
+          next(err);
+        });
+      }
+    });
+  }
+}
