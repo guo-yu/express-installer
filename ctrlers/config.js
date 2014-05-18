02d78a990cb3e3c6f83b31f712a158ce346f5a67
Guo Yu <i@guoyu.me>
2014-05-18 14:50:11 +0800

    0.0.2: bug fixed

--- ctrlers/config.js
+++ ctrlers/config.js
@@ -1,5 +1,8 @@
 module.exports = function(config) {
-  var Config = {}
+  var Config = {};
+  Config.create = function(baby, callback) {
+    return config.create(baby, callback);
+  };
   Config.check = function(callback) {
     config.findOne({}).exec(function(err, c) {
       callback(err, !!c, c);
